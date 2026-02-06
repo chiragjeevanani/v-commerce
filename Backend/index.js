0fdb87c97d984906afacf30698e88291df7d7d54
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-02-06 23:29:43 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ app.use("/api", routes);
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 // SPA fallback
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
