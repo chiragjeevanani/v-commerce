b29a7bead721bf57a344dd9427c3eaff67addb6e
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-02-17 16:54:07 +0530

    base url

--- frontend/dist/assets/minus-Bv485YlP.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./index-BOPlgZ3J.js";const s=[["path",{d:"M5 12h14",key:"1ays0h"}]],e=o("minus",s);export{e as M};
---
+++ frontend/dist/assets/minus-DY-jubIb.js
@@ -0,0 +1,1 @@
+import{q as o}from"./index-Wtch0-zL.js";const s=[["path",{d:"M5 12h14",key:"1ays0h"}]],e=o("minus",s);export{e as M};
